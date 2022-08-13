378694b28413695c8a55c2b1e519993f0fd57bfe
Adrian Cañete <canete.adrian@gmail.com>
2022-08-13 14:38:27 -0300

    Updates

---
+++ precache-manifest.ee814370302fc2625b3c55189ca71d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "98ea9785176047f9682f",
+    "url": "/react-state-machine/static/css/main.e8b228b9.chunk.css"
+  },
+  {
+    "revision": "98ea9785176047f9682f",
+    "url": "/react-state-machine/static/js/main.2b9d7c52.chunk.js"
+  },
+  {
+    "revision": "c886b5a932171c041c39",
+    "url": "/react-state-machine/static/js/runtime~main.bfab65ae.js"
+  },
+  {
+    "revision": "e63d1df5465eca7f536f",
+    "url": "/react-state-machine/static/js/2.cbde6816.chunk.js"
+  },
+  {
+    "revision": "228bdd7ce1f8307b06a0e4b5dc5839e9",
+    "url": "/react-state-machine/static/media/background-travel.228bdd7c.jpg"
+  },
+  {
+    "revision": "eebe02083d7528582289918659dabae5",
+    "url": "/react-state-machine/index.html"
+  }
+];
